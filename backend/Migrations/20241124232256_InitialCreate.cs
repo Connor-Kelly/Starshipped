a162453b011ef978720b7f4100c299e625e8a98a
Connor-Kelly <connorkelly726@gmail.com>
2024-11-24 17:38:21 -0600

    add migrations initial

---
+++ backend/Migrations/20241124232256_InitialCreate.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Starships",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    URL = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Model = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Manufacturer = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CostInCredits = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Length = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MaxAtmosphericSpeed = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Crew = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Passengers = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CargoCapacity = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Consumables = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    HyprdriveRating = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MGLT = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StarshipClass = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Films = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Pilots = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Created = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Edited = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Starships", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Starships");
+        }
+    }
+}
