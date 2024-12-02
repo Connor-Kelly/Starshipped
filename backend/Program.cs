e94cbdc37ff08d1de08aa55c548100b863038de8
Connor-Kelly <connorkelly726@gmail.com>
2024-12-01 19:48:25 -0600

    big mod, added post and put endpoints

--- backend/Program.cs
+++ backend/Program.cs
@@ -4,8 +4,9 @@ using Microsoft.EntityFrameworkCore;
 
 using System.Text.Json;
 using System.Net.Http.Json;
-using System.Text.Json;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddCors();
+// builder.Services.AddAuthentication();
+
 
 builder.Services.AddDbContext<StarWarsContext>(options =>
 {
@@ -28,9 +31,22 @@ var app = builder.Build();
 using (var scope =
   app.Services.CreateScope())
 using (var context = scope.ServiceProvider.GetService<StarWarsContext>())
-    context?.Database.EnsureCreated();
+    if (context.Database.GetPendingMigrations().Any())
+    {
+        // Attempt to Migrate the DB, When all is being scaffolded at the same 
+        // time, this has a tendancy to fail, It'll still connect...
+        try
+        {
+            context.Database.Migrate();
+        }
+        catch (Exception sqlException)
+        {
+            Console.WriteLine(sqlException);
+        }
+    }
 
-    app.UseHttpsRedirection();
+
+app.UseHttpsRedirection();
 
 // leave the CORS wide open, in prod this would need to change
 app.UseCors(builder => builder
@@ -59,13 +75,60 @@ app.MapDelete("/{id}", (int id, StarWarsContext db) =>
     return "Successfully Deleted";
 });
 
-app.MapPost("/{id}", (int id, StarWarsContext db) =>
+app.MapPost("/{id}", (int id, [FromBody] Starship ss, StarWarsContext db) =>
 {
     Starship? starship = db.Starships.Find(id);
-    db.Remove(starship);
+
+
+    Console.WriteLine("SS: ");
+    Console.WriteLine(JsonSerializer.Serialize(ss));
+
+    if (starship == null)
+    {
+        return "Could not find Starship with that ID.";
+    }
+
+    starship.name = ss.name;
+    starship.Model = ss.Model;
+    starship.Manufacturer = ss.Manufacturer;
+    starship.Pilots = ss.Pilots;
     db.SaveChanges();
 
-    return "Successfully Deleted";
+    return "Successfully Modified";
+});
+
+app.MapPut("/{id}", (int id, [FromBody] Starship ss, StarWarsContext db) =>
+{
+    Console.WriteLine(JsonSerializer.Serialize(ss));
+
+    Starship? starship = new Starship();
+    starship.name = ss.name;
+    starship.Model = ss.Model;
+    starship.Manufacturer = ss.Manufacturer;
+
+    if (ss.URL is not null)
+    {
+        starship.URL = ss.URL;
+    }
+    else
+    {
+        starship.URL = "No URL";
+    }
+    if (ss.Pilots is not null)
+    {
+        starship.Pilots = ss.Pilots;
+    }
+    else
+    {
+        starship.Pilots = new List<string>();
+    }
+    starship.Films = new List<string>();
+
+    db.Starships.Add(starship);
+    
+    db.SaveChanges();
+
+    return starship.Id;
 });
 
 
@@ -73,8 +136,6 @@ app.MapGet("/random", (StarWarsContext db) =>
         db.Starships.OrderBy((ss) => Guid.NewGuid()).First()
     )
     .WithName("GetRandomStarShip").WithOpenApi();
-
-
 
 app.Run();
 
