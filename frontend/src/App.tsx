63c79e8825cd1d0f4e02ee04a5dfc510c398dc34
Connor-Kelly <connorkelly726@gmail.com>
2024-12-01 19:50:11 -0600

    update app.tsx for other changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,63 +9,67 @@ import { Spinner } from './components/ui/spinner';
 import { Navbar } from '@/components/navbar';
 import { Route, Routes, useParams } from 'react-router-dom';
 import { EmptyStarshipCard } from './components/empty-starship-card';
+import { NewStarshipCard } from './components/new-starship-card';
+import { Toaster } from '@/components/ui/toaster';
 
 function App() {
-	const [fields, setFields] = useState<string[]>(['', '', '']);
-	
-	
 	const Main = () => {
-		const params = useParams()
+		const params = useParams();
 		const [starship, setStarship] = useState<Starship | undefined>(undefined);
 		const [loading, setIsLoading] = useState(true);
-		// axios.get('http://localhost:5119/random').then((response) => {
-		// 	setStarship(response.data);
-		// });
-		const id = params.id; 
-		console.log("id",)
-		const url = id === undefined ? "http://localhost:5119/random" : `http://localhost:5119/${id}`
+		const id = params.id;
+		console.log('id');
+		const url = id === undefined ? 'http://localhost:5119/random' : `http://localhost:5119/${id}`;
 		useMemo(() => {
-			axios.get(url).then((response) => {
-				setStarship(response.data);
-				setIsLoading(false)
-			}).catch(err => {
-				setIsLoading(false)
-			})
-		}, [])
+			axios
+				.get(url)
+				.then((response) => {
+					setStarship(response.data);
+					setIsLoading(false);
+				})
+				.catch((err) => {
+					setIsLoading(false);
+				});
+		}, []);
 		// useEffect(() => {
 		// 	id === "" ?
 		// 	axios.get('http://localhost:5119/random').then((response) => {
 		// 		setStarship(response.data);
-		// 	}) 
+		// 	})
 		// }, []);
 		return (
-			<>
-				<Navbar />
-				<div className=' flex items-center justify-center p-4 my-8'>
-					{!loading ? (
-						starship ? 
-							<StarshipCard starship={starship} />
-							: <EmptyStarshipCard />
+			<div className=' flex items-center justify-center p-4 my-8'>
+				{!loading ? (
+					starship ? (
+						<StarshipCard starship={starship} />
 					) : (
-						<Card className='h-64 w-1/2 bg-muted rounded animate-pulse items-center justify-center flex'>
-							<Spinner className='h-16 w-16' />
-						</Card>
-					)}
-				</div>
-			</>
+						<EmptyStarshipCard />
+					)
+				) : (
+					<Card className='h-64 w-1/2 bg-muted rounded animate-pulse items-center justify-center flex'>
+						<Spinner className='h-16 w-16' />
+					</Card>
+				)}
+			</div>
+		);
+	};
+	const NewCard = () => {
+		return (
+			<div className=' flex items-center justify-center p-4 my-8'>
+				<NewStarshipCard />
+			</div>
 		);
 	};
 
 	return (
 		<ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+			<Navbar />
 			<Routes>
-				<Route path='/' element={<Main />}>
-					{/* <Main /> */}
-				</Route>
-				<Route path='/:id' element={<Main />}>
-					{/* <Main /> */}
-				</Route>
+				<Route path='/' element={<Main />}></Route>
+				<Route path='/new' element={<NewCard />}></Route>
+				<Route path='/:id' element={<Main />}></Route>
 			</Routes>
+			<Toaster />
 		</ThemeProvider>
 	);
 }
