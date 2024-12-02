ed67c1ffcdf6e13266a6a8615f3abd8ac1b89a00
Connor-Kelly <connorkelly726@gmail.com>
2024-12-01 19:49:10 -0600

    add add-starship to the navbar

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,17 +1,29 @@
+import { CirclePlus } from 'lucide-react';
 
-import { Button } from '@/components/ui/button'
-import { ModeToggle } from './mode-toggle'
+import { Button } from '@/components/ui/button';
+import { ModeToggle } from './mode-toggle';
+import { useNavigate } from 'react-router-dom';
 
 export function Navbar() {
-    return (
-        <nav className="flex items-center justify-between p-4 bg-background border-b">
-            {/* <Link href="/" className="flex items-center space-x-2">
+	const navigate = useNavigate();
+	return (
+		<nav className='flex items-center justify-between p-4 bg-background border-b'>
+			{/* <Link href="/" className="flex items-center space-x-2">
                 <Home className="h-6 w-6" /> */}
-                <span className="text-xl font-semibold">Starshipped</span>
-            {/* </Link> */}
-            <div className="flex items-center space-x-4">
-                <ModeToggle />
-            </div>
-        </nav>
-    )
+			<span className='text-xl font-semibold' onClick={() => navigate('/')}>
+				Starshipped
+			</span>
+			{/* </Link> */}
+			<div className='flex items-center space-x-4'>
+				<Button
+					onClick={() => {
+						navigate('/new');
+					}}
+				>
+					<CirclePlus />
+				</Button>
+				<ModeToggle />
+			</div>
+		</nav>
+	);
 }
